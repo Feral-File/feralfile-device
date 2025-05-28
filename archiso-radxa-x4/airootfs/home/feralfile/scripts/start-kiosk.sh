17bb388ac58276b4b2fc4d6ad066b1929d55360a
Brandon Yeh <lpopo0856@gmail.com>
2025-05-28 21:14:25 +0800

    v4

--- archiso-radxa-x4/airootfs/home/feralfile/scripts/start-kiosk.sh
+++ archiso-radxa-x4/airootfs/home/feralfile/scripts/start-kiosk.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-chromium_down() {
-  echo "Stop chromium-ready.target..."
-  systemctl stop chromium-ready.target
-}
-trap chromium_down EXIT
-
 # Read saved rotation
 ROTATION="normal"
 if [ -f /home/feralfile/.config/screen-orientation ]; then
