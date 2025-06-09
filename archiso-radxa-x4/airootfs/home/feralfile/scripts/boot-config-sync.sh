c38b544d2598e85ac77a50a5e1d1d4f0ae5d36a9
Brandon Yeh <lpopo0856@gmail.com>
2025-06-09 17:30:56 +0800

    disable from kernel option directly

--- archiso-radxa-x4/airootfs/home/feralfile/scripts/boot-config-sync.sh
+++ archiso-radxa-x4/airootfs/home/feralfile/scripts/boot-config-sync.sh
@@ -38,7 +38,7 @@ title   Feral File X1
 linux   /vmlinuz-linux
 initrd  /initramfs-linux.img
 initrd  /intel-ucode.img
-options root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID rw
+options root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw
 EOF
 
 cat > /boot/loader/entries/factory_reset.conf <<EOF
@@ -46,7 +46,7 @@ title   Feral File X1 - Factory Reset
 linux   /vmlinuz-linux
 initrd  /initramfs-linux.img
 initrd  /intel-ucode.img
-options rollback=factory root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID rw
+options rollback=factory root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw
 EOF
 
 cat > /boot/loader/entries/ota_prev.conf <<EOF
@@ -54,7 +54,7 @@ title   Feral File X1 - Rollback to previous version
 linux   /vmlinuz-linux
 initrd  /initramfs-linux.img
 initrd  /intel-ucode.img
-options rollback=ota root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID rw
+options rollback=ota root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw
 EOF
 
 echo "Overwriting mkinitcpio.conf HOOKS..."
--- archiso-radxa-x4/airootfs/root/install-to-emmc.sh
+++ archiso-radxa-x4/airootfs/root/install-to-emmc.sh
@@ -165,7 +165,7 @@ title   Feral File X1
 linux   /vmlinuz-linux
 initrd  /initramfs-linux.img
 initrd  /intel-ucode.img
-options root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID rw
+options root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw
 EOF
 
 cat > /mnt/boot/loader/entries/factory_reset.conf <<EOF
@@ -173,7 +173,7 @@ title   Feral File X1 - Factory Reset
 linux   /vmlinuz-linux
 initrd  /initramfs-linux.img
 initrd  /intel-ucode.img
-options rollback=factory root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID rw
+options rollback=factory root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw
 EOF
 
 chmod 644 /mnt/boot/loader/entries/*.conf
