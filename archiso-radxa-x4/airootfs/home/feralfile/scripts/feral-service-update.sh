1024d41f0652f74af56d036d6285c4b899025a35
Brandon Yeh <lpopo0856@gmail.com>
2025-06-09 14:34:22 +0800

    better restart order

--- archiso-radxa-x4/airootfs/home/feralfile/scripts/feral-service-update.sh
+++ archiso-radxa-x4/airootfs/home/feralfile/scripts/feral-service-update.sh
@@ -6,9 +6,9 @@ output=$(sudo pacman -Sy --needed --noconfirm feral-connectd feral-setupd feral-
 if ! echo "$output" | grep -q "there is nothing to do"; then
   echo "Detected installation or upgrade. Reloading systemd and restarting services..."
   sudo systemctl daemon-reload
+  sudo systemctl restart feral-sys-monitord.service
   sudo systemctl restart feral-connectd.service
   sudo systemctl restart feral-setupd.service
-  sudo systemctl restart feral-sys-monitord.service
   sudo systemctl restart feral-watchdog.service
 else
   echo "Packages already up to date. No action needed."
