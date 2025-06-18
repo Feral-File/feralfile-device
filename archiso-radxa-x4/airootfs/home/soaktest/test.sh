8d761161a376089f6b5c499b993b31f002e481e0
Brandon Yeh <lpopo0856@gmail.com>
2025-06-18 15:33:44 +0800

    use old artwork

--- archiso-radxa-x4/airootfs/home/soaktest/test.sh
+++ archiso-radxa-x4/airootfs/home/soaktest/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ARTWORK_URL="file:///home/soaktest/uneasy-dream/index.html"
+ARTWORK_URL="file:///home/soaktest/36-point/index.html?edition_number=0&artwork_number=1&blockchain=bitmark#02_hex_hole_open"
 TEMP_VIEWER_URL="http://localhost:8000"
 
 DURATION_SECONDS=$1
