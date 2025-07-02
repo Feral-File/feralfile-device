509189012cd5013afe25051d354ade88879c87aa
Brandon Yeh <lpopo0856@gmail.com>
2025-07-02 17:43:04 +0800

    fix

--- archiso-radxa-x4/airootfs/home/soaktest/test.sh
+++ archiso-radxa-x4/airootfs/home/soaktest/test.sh
@@ -19,7 +19,7 @@ HTML_PATH="/home/soaktest/temp_viewer.html"
 
 stop() {
   echo "[INFO] Cleaning up..."
-  kill "$SERVER_PID" 2>/dev/null || true
+  kill "$SERVER_PID" "$ARTWORK_PID" 2>/dev/null || true
   echo "[INFO] Log file saved at: $LOG_FILE"
 }
 trap stop EXIT INT TERM
