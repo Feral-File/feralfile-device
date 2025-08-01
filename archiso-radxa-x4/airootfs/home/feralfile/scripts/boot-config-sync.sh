e534c8a2aba3c7ab33d2e591788f8ad544cdb67b
Brandon Yeh <lpopo0856@gmail.com>
2025-08-01 11:11:32 +0800

    add quiet boot

--- archiso-radxa-x4/airootfs/home/feralfile/scripts/boot-config-sync.sh
+++ archiso-radxa-x4/airootfs/home/feralfile/scripts/boot-config-sync.sh
@@ -51,7 +51,7 @@ title   Feral File X1
 linux   /vmlinuz-linux
 initrd  /initramfs-linux.img
 initrd  /intel-ucode.img
-options root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw
+options root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw quiet loglevel=3 systemd.show_status=auto rd.udev.log_level=3
 EOF
 
 cat > /boot/loader/entries/factory_reset.conf <<EOF
@@ -59,7 +59,7 @@ title   Feral File X1 - Factory Reset
 linux   /vmlinuz-linux
 initrd  /initramfs-linux.img
 initrd  /intel-ucode.img
-options rollback=factory root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw
+options rollback=factory root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw quiet loglevel=3 systemd.show_status=auto rd.udev.log_level=3
 EOF
 
 cat > /boot/loader/entries/ota_prev.conf <<EOF
@@ -67,7 +67,7 @@ title   Feral File X1 - Rollback to previous version
 linux   /vmlinuz-linux
 initrd  /initramfs-linux.img
 initrd  /intel-ucode.img
-options rollback=ota root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw
+options rollback=ota root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw quiet loglevel=3 systemd.show_status=auto rd.udev.log_level=3
 EOF
 
 log_info "Overwriting mkinitcpio.conf HOOKS..."
--- archiso-radxa-x4/airootfs/root/install-to-disk.sh
+++ archiso-radxa-x4/airootfs/root/install-to-disk.sh
@@ -239,7 +239,7 @@ title   Feral File X1
 linux   /vmlinuz-linux
 initrd  /initramfs-linux.img
 initrd  /intel-ucode.img
-options root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw
+options root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw quiet loglevel=3 systemd.show_status=auto rd.udev.log_level=3
 EOF
 
 cat > /mnt/boot/loader/entries/factory_reset.conf <<EOF
@@ -247,7 +247,7 @@ title   Feral File X1 - Factory Reset
 linux   /vmlinuz-linux
 initrd  /initramfs-linux.img
 initrd  /intel-ucode.img
-options rollback=factory root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw
+options rollback=factory root=PARTUUID=$PARTUUID root_partuuid=$PARTUUID ipv6.disable=1 rw quiet loglevel=3 systemd.show_status=auto rd.udev.log_level=3
 EOF
 
 chmod 644 /mnt/boot/loader/entries/*.conf
